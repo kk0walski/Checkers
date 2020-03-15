4ed906a79b33d15fa23dda121f9af86606e297cf
kk0walski <karol.kowalski95@gmail.com>
2020-03-15 12:37:01 +0100

    Refactor state and action

--- state.py
+++ state.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 class State:
     def __init__(self, board=None, turn=BLUE, hop=False):
-        if board
+        if board:
             self.board = deepcopy(board)
         else:
             self.board = Board()
@@ -15,44 +15,18 @@ class State:
         possible_moves = []
         for i in range(8):
             for j in range(8):
-                if (self.board.legal_moves(i, j, self.game.hop) != []
+                if (self.board.legal_moves(i, j, self.hop) != []
                         and self.board.location(i, j).occupant is not None
-                        and self.board.location(i, j).occupant.color == self.game.turn):
-                    for end_pos in self.board.legal_moves(i, j, self.game.hop):
+                        and self.board.location(i, j).occupant.color == self.turn):
+                    for end_pos in self.board.legal_moves(i, j, self.hop):
                         possible_moves.append(Action((i,j), end_pos))
         return possible_moves
 
     def takeAction(self, action):
-        current_pos = action.current_pos
-        final_pos = action.final_pos
-        if current_pos is None:
-            self.end_turn()
-            print(str(self.board))
-            # print(self._generate_all_possible_moves(board))
-        print(current_pos, final_pos, self.board.location(current_pos[0], current_pos[1]).occupant)
-        if not self.hop:
-            if self.board.location(final_pos[0], final_pos[1]).occupant is not None \
-                    and self.board.location(final_pos[0], final_pos[1]).occupant.color == self.turn:
-                current_pos = final_pos
+        newState = deepcopy(self)
+        action.perform(newState)
+        return newState
 
-            elif current_pos is not None and final_pos in self.board.legal_moves(current_pos[0], current_pos[1]):
-
-                self.board.move_piece(
-                    current_pos[0], current_pos[1], final_pos[0], final_pos[1])
-
-                if final_pos not in self.board.adjacent(current_pos[0], current_pos[1]):
-                    self.board.remove_piece(current_pos[0] + (final_pos[0] - current_pos[0]) //
-                                       2, current_pos[1] + (final_pos[1] - current_pos[1]) // 2)
-                    self.hop = True
-                    current_pos = final_pos
-                    final_pos = self.board.legal_moves(current_pos[0], current_pos[1], True)
-                    if final_pos:
-                        print("HOP in Action", current_pos, final_pos)
-                        self.takeAction(current_pos, final_pos[0])
-                    self.end_turn()
-
-            new_state = deepcopy(self)
-            return new_state
 
 
     def isTerminal(self):
@@ -101,3 +75,50 @@ class Action:
     def __init__(self, start_pos, end_pos):
         self.current_pos = start_pos
         self.final_pos = end_pos
+
+    def perform(self, state):
+        if self.current_pos is None:
+           state.end_turn()
+            # board.repr_matrix()
+            # print(self._generate_all_possible_moves(board))
+        # print(current_pos, final_pos, board.location(current_pos[0], current_pos[1]).occupant)
+        if state.hop == False:
+            if state.board.location(self.final_pos[0], self.final_pos[1]).occupant != None and\
+                    state.board.location(self.final_pos[0], self.final_pos[1]).occupant.color == state.turn:
+                self.current_pos = self.final_pos
+
+            elif self.current_pos != None and final_pos in state.board.legal_moves(self.current_pos[0], self.current_pos[1]):
+
+                state.board.move_piece(
+                    self.current_pos[0], self.current_pos[1], self.final_pos[0], self.final_pos[1])
+
+                if self.final_pos not in state.board.adjacent(self.current_pos[0], self.current_pos[1]):
+                    state.board.remove_piece(self.current_pos[0] + (self.final_pos[0] - self.current_pos[0]) //
+                                       2, self.current_pos[1] + (self.final_pos[1] - self.current_pos[1]) // 2)
+
+                    state.hop = True
+                    self.current_pos = self.final_pos
+                    self.final_pos = state.board.legal_moves(
+                        self.current_pos[0], self.current_pos[1], True)
+                    if final_pos != []:
+                        # print("HOP in Action", current_pos, final_pos)
+                        self.performAction(self.current_pos, self.final_pos[0], state)
+                    self.state.end_turn()
+            state.turn = RED if state.turn == BLUE else BLUE
+        else:
+            if self.current_pos != None and self.final_pos in state.board.legal_moves(self.current_pos[0], self.current_pos[1], state.hop):
+                state.board.move_piece(
+                    self.current_pos[0], self.current_pos[1], self.final_pos[0], self.final_pos[1])
+                state.board.remove_piece(self.current_pos[0] + (self.final_pos[0] - self.current_pos[0]) //
+                                   2, self.current_pos[1] + (self.final_pos[1] - self.current_pos[1]) // 2)
+
+            if state.board.legal_moves(self.final_pos[0], self.final_pos[1], state.hop) == []:
+                state.end_turn()
+            else:
+                self.current_pos = self.final_pos
+                self.final_pos = state.board.legal_moves(
+                    self.current_pos[0], self.current_pos[1], True)
+                if self.final_pos != []:
+                    # print("HOP in Action", current_pos, final_pos)
+                    self.performAction(self.current_pos, self.final_pos[0], state)
+                state.end_turn()
